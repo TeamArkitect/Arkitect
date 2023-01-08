fa3e6e85ae2a123ceb7fe5fb51425a17f08c9ffe
devlaq <devlaq4918@gmail.com>
2023-01-08 19:00:35 +0900

    Now script generates plugin.json from plugin.template.json automatically, so added plugin.json to .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,30 @@ group = "com.github.devlaq"
 version = "1.0-SNAPSHOT"
 
 val mindustryVersion: String by project
-
 val testServerDirectory: String? by project
+
+val pluginVersion: String? by project
+
+/**
+ * Generate plugin.json
+ */
+fun generatePluginMeta() {
+    val template = File("./plugin.template.json")
+    val dest = File("./src/main/resources/plugin.json")
+
+    val placeholders = mapOf<String, Any?>(
+        "version" to pluginVersion
+    )
+
+    var replaced = template.readText()
+    placeholders.forEach { (t, u) ->
+        replaced = replaced.replace("\${${t}}", u.toString())
+    }
+
+    if(!dest.exists()) dest.createNewFile()
+    dest.writeText(replaced)
+}
+generatePluginMeta()
 
 tasks.test {
     useJUnitPlatform()
@@ -26,7 +48,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.github.devlaq.arkitect"
             artifactId = "Arkitect"
-            version = "dev0.1"
+            version = pluginVersion
 
             from(components["java"])
         }
@@ -61,54 +83,56 @@ val fatJar = task("fatJar", type = Jar::class) {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
-task("testFatJar") {
-    if(!fatJar.archiveFile.get().asFile.exists()) return@task
-    val buildDir = layout.buildDirectory
-    val builtFatJar = buildDir.dir("libs").get().file(fatJar.archiveFileName.get()).asFile
-    val destFile = File("${testServerDirectory}/config/mods/Arkitect-TEST.jar")
-    if(testServerDirectory != null) {
-        doLast {
-            builtFatJar.copyTo(destFile, overwrite = true)
+task("generateSources") {
+    generatePluginMeta()
+}
 
-            fun getJvmProcesses(): MutableMap<Int, String> {
-                val jpsProcess = ProcessBuilder()
-                    .command("/usr/lib/jvm/jdk-17/bin/jps", "-l")
-                    .start()
-                jpsProcess.waitFor()
-                val output = jpsProcess.inputReader().readText()
-                val map = mutableMapOf<Int, String>()
-                output.lines().forEach {
-                    val split = it.split(" ", limit = 2)
-                    if(split.size > 1) map[split[0].toInt()] = split[1]
+if(System.getenv("JITPACK") != "true") {
+    task("testFatJar") {
+        dependsOn(fatJar)
+        if(!fatJar.archiveFile.get().asFile.exists()) return@task
+        val buildDir = layout.buildDirectory
+        val builtFatJar = buildDir.dir("libs").get().file(fatJar.archiveFileName.get()).asFile
+        val destFile = File("${testServerDirectory}/config/mods/Arkitect-TEST.jar")
+        if(testServerDirectory != null) {
+            doLast {
+                builtFatJar.copyTo(destFile, overwrite = true)
+
+                fun getJvmProcesses(): MutableMap<Int, String> {
+                    val jpsProcess = ProcessBuilder()
+                        .command("/usr/lib/jvm/jdk-17/bin/jps", "-l")
+                        .start()
+                    jpsProcess.waitFor()
+                    val output = jpsProcess.inputReader().readText()
+                    val map = mutableMapOf<Int, String>()
+                    output.lines().forEach {
+                        val split = it.split(" ", limit = 2)
+                        if(split.size > 1) map[split[0].toInt()] = split[1]
+                    }
+                    return map
                 }
-                return map
-            }
-            fun killAliveTestServers() {
-                val jvmProcesses = getJvmProcesses().filterValues { it.startsWith(testServerDirectory!!) }
-                val processesToKill = jvmProcesses.keys.joinToString(" ") { it.toString() }
-                println("Killed alive test servers [${jvmProcesses.keys.joinToString(",")}]")
+                fun killAliveTestServers() {
+                    val jvmProcesses = getJvmProcesses().filterValues { it.startsWith(testServerDirectory!!) }
+                    val processesToKill = jvmProcesses.keys.joinToString(" ") { it.toString() }
+                    println("Killed alive test servers [${jvmProcesses.keys.joinToString(",")}]")
+                    val process = ProcessBuilder()
+                        .command("kill", "-9", processesToKill)
+                        .start()
+                    process.waitFor()
+                }
+                killAliveTestServers()
                 val process = ProcessBuilder()
-                    .command("kill", "-9", processesToKill)
+                    .command("gnome-terminal", "--title", "Mindustry v$mindustryVersion Headless [Arkitect Testing Server]", "--wait", "--", "sh", "${testServerDirectory}/start.sh")
+                    .directory(File(testServerDirectory!!))
                     .start()
                 process.waitFor()
-                println(process.inputReader().readText())
             }
-            killAliveTestServers()
-            val process = ProcessBuilder()
-                .command("gnome-terminal", "--title", "Mindustry v$mindustryVersion Headless [Arkitect Testing Server]", "--wait", "--", "sh", "${testServerDirectory}/start.sh")
-                .directory(File(testServerDirectory!!))
-                .start()
-            process.waitFor()
         }
-
     }
 }
 
 tasks {
     "build" {
         dependsOn(fatJar)
-    }
-    "testFatJar" {
-        dependsOn(build)
     }
 }
